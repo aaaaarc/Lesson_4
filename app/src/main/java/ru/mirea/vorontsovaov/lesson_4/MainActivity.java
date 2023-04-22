48bfbdb02026704712016fb4e3cd14fc0882a841
aaaaarc <re.lumence@gmail.com>
2023-04-22 23:43:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/vorontsovaov/lesson_4/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.mirea.vorontsovaov.lesson_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+import ru.mirea.vorontsovaov.lesson_4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private TextView textView;
+    private ImageView imageView1;
+    private ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        textView = binding.textView;
+        imageView1 = binding.imageView;
+        imageView2 = binding.imageView;
+    }
+}
